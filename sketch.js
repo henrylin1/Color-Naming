0d088017351597cd5968c1c35531abe345664018
henrylin1 <33580556+henrylin1@users.noreply.github.com>
2018-12-28 18:05:30 -0500

    able to read/write to firebase

--- sketch.js
+++ sketch.js
@@ -1,23 +1,66 @@
+let r, g, b;
+let database;
+
+
 function setup() {
+  // Initialize Firebase
+	var config = {
+	apiKey: "AIzaSyCxG3ZkG_MesIAIL62EPjc9znnGevNQX4Y",
+	authDomain: "fir-test-7a709.firebaseapp.com",
+	databaseURL: "https://fir-test-7a709.firebaseio.com",
+	projectId: "fir-test-7a709",
+	storageBucket: "fir-test-7a709.appspot.com",
+	messagingSenderId: "954618786053"
+	};
+	firebase.initializeApp(config);
+	database = firebase.database();
+	console.log(firebase);
+
 	createCanvas(100, 100);
-	let r = floor(random(256));
-	let g = floor(random(256));
-	let b = floor(random(256));
+	r = floor(random(256));
+	g = floor(random(256));
+	b = floor(random(256));
 	background(r, g, b);
 
-	let dropdown = createRadio();
-	dropdown.option("Yellow");
-	dropdown.option("Red");
-	dropdown.option("Blue");
-	dropdown.option("Orange");
-	dropdown.option("Purple");
-	dropdown.option("Green");
-	dropdown.option("Brown");
+	let buttons = [];
+	buttons.push(createButton("Yellow"));
+	buttons.push(createButton("Red"));
+	buttons.push(createButton("Blue"));
+	buttons.push(createButton("Orange"));
+	buttons.push(createButton("Purple"));
+	buttons.push(createButton("Green"));
+	buttons.push(createButton("Brown"));
 
-	let submit = createButton('submit');
-	submit.mousePressed(sendData);
-
-	function sendData() {
-		pass;
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].mousePressed(sendData);
 	}
+}
+
+function sendData() {
+	let colorDatabase = database.ref("colors");
+
+	//make object to be sent to database
+	var data = {
+		r: r,
+		g: g,
+		b: b,
+		label: this.html()
+	}
+
+	let color = colorDatabase.push(data, errorCheck);
+	console.log("firebase generated key: " + color.key);
+	function errorCheck(error) {
+		if (error) {
+			console.error(error);
+		}
+		else{
+			r = floor(random(256));
+			g = floor(random(256));
+			b = floor(random(256));
+			background(r, g, b);
+		}
+	}
+
+	console.log(this.html());
+
 }
