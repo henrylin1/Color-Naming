a7dd9d8dd2224a21436cb330ca4b897d94fbc43b
henrylin1 <33580556+henrylin1@users.noreply.github.com>
2018-12-29 23:27:31 -0500

    basic classifier finished

---
+++ classifier/sketch.js
@@ -0,0 +1,121 @@
+var data;
+var start;
+let labelList = ["Yellow", "Red", "Blue", "Orange", "Purple", "Green"];
+let model;
+let x;
+let y;
+let lossP;
+let labelP;
+let rSlider;
+let gSlider; 
+let bSlider;
+
+function gotData(results) {
+	let inputData = results.val();
+	let outputData = {
+		colors: []
+	};
+
+	let keys = Object.keys(inputData);
+	for (let key of keys) {
+		let record = inputData[key];
+		outputData.colors.push(record);
+	}
+	data = outputData;
+	saveJSON(data, "colorData.json");
+}
+
+async function setup() {
+	lossP = createP("loss");
+	labelP = createP("guess");
+	rSlider = createSlider(0, 255, 255);
+	gSlider = createSlider(0, 255, 255);
+	bSlider = createSlider(0, 255, 0);
+	// Initialize Firebase
+	var config = {
+	apiKey: "AIzaSyCxG3ZkG_MesIAIL62EPjc9znnGevNQX4Y",
+	authDomain: "fir-test-7a709.firebaseapp.com",
+	databaseURL: "https://fir-test-7a709.firebaseio.com",
+	projectId: "fir-test-7a709",
+	storageBucket: "fir-test-7a709.appspot.com",
+	messagingSenderId: "954618786053"
+	};
+	firebase.initializeApp(config);
+	database = firebase.database();
+	let ref = database.ref('colors');
+	start = ref.once('value', gotData);
+	await start;
+	let colors = [];
+	let labels = [];
+	for (let record of data.colors) {
+		let col = [record.r/255, record.g/255, record.b/255];
+		colors.push(col);
+		labels.push(labelList.indexOf(record.label));
+	}
+
+	x = tf.tensor2d(colors);
+
+	let labelsTensor =tf.tensor1d(labels, "int32");
+
+	y = tf.oneHot(labelsTensor, 6);
+	labelsTensor.dispose();
+
+	model = tf.sequential();
+
+	let hidden = tf.layers.dense({
+		units: 15,
+		activation: "sigmoid",
+		inputDim: 3
+	});
+
+	let output = tf.layers.dense({
+		units: 6,
+		activation: "softmax"
+	})
+	model.add(hidden);
+	model.add(output);
+
+	const optimizer = tf.train.sgd(0.2);
+
+	model.compile({
+		optimizer: optimizer,
+		loss: "categoricalCrossentropy",
+	});
+	train().then(results => {
+		console.log(results.history.loss);
+	});
+}
+
+async function train() {
+	const options = {
+		epochs: 50,
+		validationSplit: 0.2,
+		shuffle: true,
+		callbacks: {
+			onTrainBegin: () => console.log("training started"),
+			onTrainEnd: () => console.log("training complete"),
+			onBatchEnd: tf.nextFrame,
+			onEpochEnd: (num, log) => {
+				console.log("Epoch: " + num);
+				lossP.html("Loss: " + log.loss);
+			}
+		}
+	}
+	return await model.fit(x, y, options);
+}
+
+async function draw() {
+	await start;
+	let r = rSlider.value();
+	let g = gSlider.value();
+	let b = bSlider.value();
+	background(r,g,b);
+
+	const x = tf.tensor2d([
+		[r/255, g/255, b/255]
+	]);
+	let results = model.predict(x);
+	let index = results.argMax(1).dataSync()[0];
+	labelP.html("Label: " + labelList[index]);
+	
+}
--- sketch.js
+++ sketch.js
@@ -29,7 +29,6 @@ function setup() {
 	buttons.push(createButton("Orange"));
 	buttons.push(createButton("Purple"));
 	buttons.push(createButton("Green"));
-	buttons.push(createButton("Brown"));
 	let button = createButton("generate!");
 	button.mousePressed(generate);
 
@@ -39,7 +38,7 @@ function setup() {
 }
 
 function sendData() {
-	let colorDatabase = database.ref("colors");
+	let colorDatabase = database.ref("colors");		
 
 	//make object to be sent to database
 	var data = {
@@ -65,9 +64,60 @@ function sendData() {
 
 	console.log(this.html());
 }
+//red: 1, 0.3, 0.5 if more green, orange if more blue, purple
+//green: 0.7, 1, 0.7 if more blue, blue if more red, yellow
+//blue: 0.3, 1, 1 if more red, purple
+function guess(r, g, b) {
+	var intensity = Math.max(r, g, b);
+	if (r == intensity) {
+		if (g > b) {
+			if ( g/intensity > 0.4) {
+				if (g/intensity > 0.8) {
+					return "Yellow";
+				}
+				return "Orange";
+			}
+		}
+		else {
+			if (b/intensity > 0.5) {
+				return "Purple";
+			}
+		}
+		return "Red";
+	}
 
+	else if (g == intensity) {
+		if (r > b) {
+			if ( r/intensity > 0.6) {
+				return "Yellow";
+			}
+		}
+		else {
+			if (b/intensity > 0.7) {
+				return "Blue";
+			}
+		}
+		return "Green";
+	}
+
+	else {
+		if (r > g) {
+			if ( r/intensity > 0.3) {
+				return "Purple";
+			}
+		}
+		return "Blue";
+	}
+}
 function generate() {
-	colors = [[1,2,3,"blue"], [2,3,4,"green"]];
+	var colors = []
+	for (let i = 0; i < 100; i++){
+		r = floor(random(256));
+		g = floor(random(256));
+		b = floor(random(256));
+		var label = guess(r, g, b);
+		colors.push([r, g, b, label])
+	}
 	for (let i = 0; i < colors.length; i++) {
 		console.log(i);
 		console.log(colors[i]);
